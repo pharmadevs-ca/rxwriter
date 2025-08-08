2ffca36f0eea38e74a7932183bb8eacbabb16b0f
Alan Choy <alan.choyz@gmail.com>
2025-08-07 21:45:10 -0600

    sidebar name

--- components/app-sidebar.js
+++ components/app-sidebar.js
@@ -45,7 +45,7 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>RxWriter</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
