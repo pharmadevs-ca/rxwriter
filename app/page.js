b0e2310731a692ce5a0750cf94dab401ed510f6b
Nicole Lim Tung <nlimtung@gmail.com>
2025-08-21 20:52:33 -0400

    adding input fields for medication info

--- app/page.js
+++ app/page.js
@@ -1,7 +1,35 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import PrescriptionInputs from "@/components/prescription-inputs";
+import GeneratedPrescription from "@/components/generated-prescription";
+
 export default function Home() {
+  const [prescriptionData, setPrescriptionData] = useState();
+  const [showGeneratedPrescription, setShowGeneratedPrescription] =
+    useState(false);
+
   return (
     <div>
       <h1>Welcome to RxWriter</h1>
+
+      <PrescriptionInputs
+        prescriptionData={prescriptionData}
+        setPrescriptionData={setPrescriptionData}
+      />
+
+      <Button
+        onClick={() => {
+          setShowGeneratedPrescription(!showGeneratedPrescription);
+        }}
+        variant="secondary"
+      >
+        Generate Prescription
+      </Button>
+      <br />
+      <br />
+
+      {showGeneratedPrescription && <GeneratedPrescription />}
     </div>
-  )
+  );
 }
