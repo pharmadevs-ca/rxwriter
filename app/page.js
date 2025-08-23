c1c33216a29c506d0ca71a21bcae78bc57a3b8ba
Alan Choy <alan.choyz@gmail.com>
2025-08-22 22:34:03 -0600

    added maxwidth to the parent div and generated rx grid box

--- app/page.js
+++ app/page.js
@@ -12,37 +12,39 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to RxWriter</h1>
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <PrescriptionInputs
-            prescriptionData={prescriptionData}
-            setPrescriptionData={setPrescriptionData}
-          />
+      <div style={{ padding: '20px', maxWidth: '1200px', margin: '0 auto' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <PrescriptionInputs
+              prescriptionData={prescriptionData}
+              setPrescriptionData={setPrescriptionData}
+            />
+            <Button
+              onClick={() => {
+                setShowGeneratedPrescription(!showGeneratedPrescription)
+              }}
+              variant='contained'
+              sx={{ mt: 2 }}
+            >
+              Generate Prescription
+            </Button>
+          </Grid>
 
-          <Button
-            onClick={() => {
-              setShowGeneratedPrescription(!showGeneratedPrescription)
-            }}
-            variant='contained'
-            sx={{ mt: 2 }}
-          >
-            Generate Prescription
-          </Button>
+          <Grid item xs={12} md={6}>
+            {showGeneratedPrescription && (
+              <div
+                style={{
+                  maxWidth: '600px',
+                  wordBreak: 'break-word',
+                  overflowWrap: 'break-word',
+                }}
+              >
+                <GeneratedPrescription />
+              </div>
+            )}
+          </Grid>
         </Grid>
-        <br />
-        <br />
-        <Grid
-          item
-          xs={6}
-          sx={{
-            maxWidth: '600px',
-            wordbreak: 'break-word',
-            overflowWrap: 'break-word',
-          }}
-        >
-          {showGeneratedPrescription && <GeneratedPrescription />}
-        </Grid>
-      </Grid>
+      </div>
     </div>
   )
 }
