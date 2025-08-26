6d2f562016818c60ed2824464e9426a50be538a5
Edmond Chiu <dev@edmondchiu.ca>
2025-08-25 20:12:23 -0400

    add typescript checking to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  ...compat.extends("plugin:@typescript-eslint/recommended"),
+];
 
 export default eslintConfig;
