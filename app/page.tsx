f367ec961929262f4013cfeb41f8ee188c3ee120
Edmond Chiu <dev@edmondchiu.ca>
2025-08-25 20:30:20 -0400

    update to MUI grid v2

--- app/page.tsx
+++ app/page.tsx
@@ -13,37 +13,33 @@ export default function Home() {
     <div>
       <h1>Welcome to RxWriter</h1>
       <div style={{ padding: "20px", maxWidth: "1200px", margin: "0 auto" }}>
-        <Grid container spacing={3}>
-          {/*TODO: upgrade to grid v2 */}
-          <Grid item xs={12} md={6}>
-            <PrescriptionInputs
-              prescriptionData={prescriptionData}
-              setPrescriptionData={setPrescriptionData}
-            />
-            <Button
-              onClick={() => {
-                setShowGeneratedPrescription(!showGeneratedPrescription);
+        <Grid size={{ xs: 12, md: 6 }}>
+          <PrescriptionInputs
+            prescriptionData={prescriptionData}
+            setPrescriptionData={setPrescriptionData}
+          />
+          <Button
+            onClick={() => {
+              setShowGeneratedPrescription(!showGeneratedPrescription);
+            }}
+            variant="contained"
+            sx={{ mt: 2 }}
+          >
+            Generate Prescription
+          </Button>
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          {showGeneratedPrescription && (
+            <div
+              style={{
+                maxWidth: "600px",
+                wordBreak: "break-word",
+                overflowWrap: "break-word",
               }}
-              variant="contained"
-              sx={{ mt: 2 }}
             >
-              Generate Prescription
-            </Button>
-          </Grid>
-          {/*TODO: upgrade to grid v2 */}
-          <Grid item xs={12} md={6}>
-            {showGeneratedPrescription && (
-              <div
-                style={{
-                  maxWidth: "600px",
-                  wordBreak: "break-word",
-                  overflowWrap: "break-word",
-                }}
-              >
-                <GeneratedPrescription />
-              </div>
-            )}
-          </Grid>
+              <GeneratedPrescription />
+            </div>
+          )}
         </Grid>
       </div>
     </div>
