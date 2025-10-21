2e1164c950ed965f93d0f027a5f12bbd3e74cec9
Edmond Chiu <dev@edmondchiu.ca>
2025-10-21 19:49:08 -0400

    fix merge

--- components/prescription-inputs.tsx
+++ components/prescription-inputs.tsx
@@ -29,99 +29,97 @@ export default function PrescriptionInputs({
       dose: medication.strength,
       dosageForm: medication.dosageForm,
     });
-    const handlePrescriptionChange = (
-      e: React.ChangeEvent<HTMLInputElement>,
-    ) => {
-      setPrescriptionData({
-        ...prescriptionData,
-        [e.target.id]: e.target.value,
-      });
-    };
+  };
+  const handlePrescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrescriptionData({
+      ...prescriptionData,
+      [e.target.id]: e.target.value,
+    });
+  };
 
-    const handlePharmacistChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setPharmacistData({ ...pharmacistData, [e.target.id]: e.target.value });
-    };
+  const handlePharmacistChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPharmacistData({ ...pharmacistData, [e.target.id]: e.target.value });
+  };
 
-    return (
-      <div className="flex flex-col gap-4">
-        {/* Medication search/selection component */}
-        <MedicationAutocomplete
-          onSelect={handleMedicationSelect}
-          onCustomModeChange={setIsCustomMode}
-        />
+  return (
+    <div className="flex flex-col gap-4">
+      {/* Medication search/selection component */}
+      <MedicationAutocomplete
+        onSelect={handleMedicationSelect}
+        onCustomModeChange={setIsCustomMode}
+      />
 
-        {/* Dose field - only shown in custom drug entry mode
+      {/* Dose field - only shown in custom drug entry mode
           In API mode, dose is already displayed in the selected medication card */}
-        {isCustomMode && (
-          <TextField
-            id="dose"
-            label="Dose"
-            variant="outlined"
-            value={prescriptionData?.dose || ""}
-            onChange={handleChange}
-          />
-        )}
-
-        {/* Sig (Signatura) - dosing instructions for the patient */}
-        <TextField
-          id="medicationName"
-          label="Medication Name"
-          variant="outlined"
-          value={prescriptionData.medicationName || ""}
-          onChange={handlePrescriptionChange}
-        />
-
+      {isCustomMode && (
         <TextField
           id="dose"
           label="Dose"
           variant="outlined"
-          value={prescriptionData.dose || ""}
-          onChange={handlePrescriptionChange}
+          value={prescriptionData?.dose || ""}
+          onChange={handleChange}
         />
-        <TextField
-          id="sig"
-          label="Sig"
-          variant="outlined"
-          value={prescriptionData.sig || ""}
-          onChange={handlePrescriptionChange}
-        />
+      )}
 
-        {/* Mitte - quantity to dispense */}
-        <TextField
-          id="mitte"
-          label="Mitte"
-          variant="outlined"
-          value={prescriptionData.mitte || ""}
-          onChange={handlePrescriptionChange}
-        />
+      {/* Sig (Signatura) - dosing instructions for the patient */}
+      <TextField
+        id="medicationName"
+        label="Medication Name"
+        variant="outlined"
+        value={prescriptionData.medicationName || ""}
+        onChange={handlePrescriptionChange}
+      />
 
-        {/* Number of refills allowed */}
-        <TextField
-          id="refills"
-          label="Refills"
-          variant="outlined"
-          value={prescriptionData.refills || ""}
-          onChange={handlePrescriptionChange}
-        />
+      <TextField
+        id="dose"
+        label="Dose"
+        variant="outlined"
+        value={prescriptionData.dose || ""}
+        onChange={handlePrescriptionChange}
+      />
+      <TextField
+        id="sig"
+        label="Sig"
+        variant="outlined"
+        value={prescriptionData.sig || ""}
+        onChange={handlePrescriptionChange}
+      />
 
-        <br />
+      {/* Mitte - quantity to dispense */}
+      <TextField
+        id="mitte"
+        label="Mitte"
+        variant="outlined"
+        value={prescriptionData.mitte || ""}
+        onChange={handlePrescriptionChange}
+      />
 
-        {/* Pharmacist Information */}
-        <TextField
-          id="pharmacistName"
-          label="Name"
-          variant="outlined"
-          value={pharmacistData.pharmacistName || ""}
-          onChange={handlePharmacistChange}
-        />
-        <TextField
-          id="licenseNumber"
-          label="License Number"
-          variant="outlined"
-          value={pharmacistData.licenseNumber || ""}
-          onChange={handlePharmacistChange}
-        />
-      </div>
-    );
-  };
+      {/* Number of refills allowed */}
+      <TextField
+        id="refills"
+        label="Refills"
+        variant="outlined"
+        value={prescriptionData.refills || ""}
+        onChange={handlePrescriptionChange}
+      />
+
+      <br />
+
+      {/* Pharmacist Information */}
+      <TextField
+        id="pharmacistName"
+        label="Name"
+        variant="outlined"
+        value={pharmacistData.pharmacistName || ""}
+        onChange={handlePharmacistChange}
+      />
+      <TextField
+        id="licenseNumber"
+        label="License Number"
+        variant="outlined"
+        value={pharmacistData.licenseNumber || ""}
+        onChange={handlePharmacistChange}
+      />
+    </div>
+  );
 }
